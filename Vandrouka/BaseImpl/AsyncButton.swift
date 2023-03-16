df163a345ac26b9deb5f42e4349d1b468beaf4d7
Arseni <arsenii12345@yandex.ru>
2023-03-16 23:16:53 +0300

    added: base view implementations for buttons

---
+++ Vandrouka/BaseImpl/AsyncButton.swift
@@ -0,0 +1,68 @@
+//
+//  AsyncButton.swift
+//  Vandrouka
+//
+//  Created by Arseni Laputska on 16.03.23.
+//
+
+import SwiftUI
+
+struct AsyncButton<Label: View>: View {
+    
+    var action: () async -> Void
+    var actionOptions = Set(ActionOption.allCases)
+    @ViewBuilder var label: () -> Label
+    
+    @State private var isDisabled = false
+    @State private var showProgressView = false
+    
+    var body: some View {
+        Button(action: {
+            if actionOptions.contains(.disableButton) {
+                isDisabled.toggle()
+            }
+            
+            Task {
+                var progressViewTask: Task<Void, Error>?
+                
+                if actionOptions.contains(.showProgressView) {
+                    progressViewTask = Task {
+                        try await Task.sleep(nanoseconds: 150_000_000)
+                        showProgressView.toggle()
+                    }
+                }
+                
+                await action()
+                progressViewTask?.cancel()
+                
+                isDisabled.toggle()
+                showProgressView.toggle()
+            }
+            
+        }, label: {
+            ZStack {
+                label()
+                    .opacity(showProgressView ? 0.5 : 1)
+                
+                if showProgressView {
+                    ProgressView()
+                        .foregroundColor(.white)
+                }
+            }
+        })
+        .disabled(isDisabled)
+    }
+}
+
+extension AsyncButton {
+    enum ActionOption: CaseIterable {
+        case disableButton
+        case showProgressView
+    }
+}
+
+extension AsyncButton where Label == Text {
+    init(@ViewBuilder label: @escaping () -> Label, actionOptions: Set<ActionOption> = Set(ActionOption.allCases), action: @escaping () async -> Void){
+        self.init(action: action, label: label)
+    }
+}
---
+++ Vandrouka/BaseImpl/NavigationBackButton.swift
@@ -0,0 +1,25 @@
+//
+//  NavigationBackButton.swift
+//  Vandrouka
+//
+//  Created by Arseni Laputska on 16.03.23.
+//
+
+import SwiftUI
+
+struct NavigationBackButton: View {
+    
+    var action: () -> Void
+    var label: String = ""
+    
+    var body: some View {
+        Button(action: action, label: {
+            HStack {
+                Image(systemName: "arrow.left")
+                    .foregroundColor(.black)
+                
+                Text(label)
+            }
+        })
+    }
+}
